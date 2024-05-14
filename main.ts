a6a3851008de19574908fc1c042f00f09a9baa20
Bernardinus Hendra Natadiria <34881602+hendranatadiria@users.noreply.github.com>
2024-05-14 17:40:56 +0700

    chore: [HN] add package.json entrypoints & port config

--- main.ts
+++ main.ts
@@ -3,8 +3,10 @@
 import express, { Request, Response } from "express";
 import Redis from "ioredis";
 import { v4 as uuidv4 } from "uuid";
+import 'dotenv/config';
 
-const redisClient = new Redis();
+
+const redisClient = new Redis(process.env.REDIS_PORT ?? 6379, process.env.REDIS_HOST ?? '127.0.0.1');
 const app = express();
 const port = 9090;
 
